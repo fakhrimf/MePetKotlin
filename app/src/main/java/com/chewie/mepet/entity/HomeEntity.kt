5eb3705ca9380894266748d6fa363fa9c359b4c0
Gilang Satria <gsatria253@gmail.com>
2019-08-30 15:07:28 +0700

    Penambahan Database(belum beres)

---
+++ app/src/main/java/com/chewie/mepet/entity/HomeEntity.kt
@@ -0,0 +1,13 @@
+package com.chewie.mepet.entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "t_home",foreignKeys = arrayOf(ForeignKey(entity = ProfileEntity::class,
+    parentColumns = arrayOf("id_profile"),childColumns = arrayOf("id_profile"))))
+data class HomeEntity (
+    @PrimaryKey(autoGenerate = true)@ColumnInfo(name="id_home") var id_home:Int,
+    @ColumnInfo(name = "id_profile") var id_profile:Int
+)
