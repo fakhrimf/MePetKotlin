b378211ebd26e23d57ae19a57b6a160c8b2a02e7
Sulthonaul <sulthon.mt@gmail.com>
2019-09-06 10:51:15 +0700

    animation

--- app/src/main/java/com/chewie/mepet/MainActivity.kt
+++ app/src/main/java/com/chewie/mepet/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this,R.anim.kekanan)
+        kiri.startAnimation(animation)
+
+        val animations = AnimationUtils.loadAnimation(this,R.anim.kekiri)
+        kanan.startAnimation(animations)
+
         mWaitHandler.postDelayed({
             try {
                 intent = Intent(applicationContext, Home::class.java)
@@ -20,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             } catch (ignored: Exception) {
                 ignored.printStackTrace()
             }
-        }, 2000)  // Delay dalam millisekon
+        }, 2600)  // Delay dalam millisekon
     }
 
     public override fun onDestroy() {
