cf6bc1039ead5e909bb0c3c10da3f6db43e87d17
Rifky Zikriansyah <zikriansyah2002@gmail.com>
2019-09-13 13:57:53 +0700

    about us

---
+++ app/src/main/java/com/chewie/mepet/aboutFrag.kt
@@ -0,0 +1,29 @@
+package com.chewie.mepet
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.CardView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_home.*
+
+class aboutFrag : Fragment() {
+    companion object {
+        fun newInstance(): aboutFrag {
+            return aboutFrag()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about_us, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
